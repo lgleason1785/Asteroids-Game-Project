1f037d9a995b94e52dbf0318eb5235f20b0ff2bf
Logan <logangleason@gmail.com>
2025-03-17 23:36:35 -0700

    Added player class and movement methods

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import pygame
 #imports global constants
 from constants import * 
+#import player class
+from player import *
 
 def main():
     pygame.init()
@@ -9,6 +11,9 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    gameClock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while(True):
         #Process input/events
@@ -16,9 +21,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         #Update game state
+        player.update(dt)
 
         #Render/Draw
         screen.fill((0,0,0)) #RGB tuple for black
+        player.draw(screen)
         pygame.display.flip() 
+
+        #Update game clock
+        dt = gameClock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
